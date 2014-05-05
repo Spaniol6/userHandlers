c7846b07424d2d94fe16927291debad4c76be851
Spaniol6 <mspaniol@yahoo.com>
2014-05-05 09:52:25 -0400

    Update userUpdater.java

--- userUpdater.java
+++ userUpdater.java
@@ -49,6 +49,7 @@ public class UserUpdater {
         Connection con;
         Random rand = new Random();
         String[] states = {"Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","DC","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennesse","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"};
+        double[] stateNum = new double[51];
         double[] s_points = new double[51];
         
         try{
@@ -80,14 +81,85 @@ public class UserUpdater {
                     inc++;
                     rs.next();
                 }
-                try {
+            
+            stmt = (PreparedStatement)con.prepareStatement("SELECT * FROM Player");
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                stmt1 = (PreparedStatement)con.prepareStatement("SELECT * FROM Lives_in WHERE p_username = '"+rs.getString("username")+"'");
+                rs1 = stmt1.executeQuery();
+                
+                rs1.next();
+                int i = 0;
+                int snum = -1;
+                while(i >= 0){
+                    if(states[i].equals(rs1.getString("s_name"))){
+                        if(i == 7){
+                            i = 8;
+                        }
+                        else if(i==8){
+                            i = 7;
+                        }
+                        snum = i;
+                        i = -2;
+                    }
+                    else if(i == 51){
+                        i = -1;
+                    }
+                    i++;
+                }
+                
+                stateNum[snum] = stateNum[snum] + rs.getDouble("e_points");  
+            }
+            
+            stmt = (PreparedStatement)con.prepareStatement("SELECT * FROM Distr ORDER BY s_name DESC");
+            rs = stmt.executeQuery();
+            
+            int i;
+            for(i=0;i<51;i++){
+                rs.next();
+                stateNum[i] = stateNum[i] + rs.getDouble("effort");
+                
+                if(i == 7){
+                    stmt1 = (PreparedStatement)con.prepareStatement("UPDATE Distr SET s_points="+stateNum[i]+" WHERE s_name = '"+states[8]+"'");
+                    stmt1.executeUpdate();
+                }
+                else if(i == 8){
+                    stmt1 = (PreparedStatement)con.prepareStatement("UPDATE Distr SET s_points="+stateNum[i]+" WHERE s_name = '"+states[7]+"'");
+                    stmt1.executeUpdate();
+                }
+                else{
+                    stmt1 = (PreparedStatement)con.prepareStatement("UPDATE Distr SET s_points="+stateNum[i]+" WHERE s_name = '"+states[i]+"'");
+                    stmt1.executeUpdate();
+                }
+            }
+            
+            stmt = (PreparedStatement)con.prepareStatement("SELECT * FROM Distr ORDER BY s_points DESC");
+            rs = stmt.executeQuery();
+            int rank = 0;
+            while(rs.next()){
+                rank++;
+                stmt1 = (PreparedStatement)con.prepareStatement("UPDATE Distr SET s_rank="+rank+" WHERE s_name = '"+rs.getString("s_name")+"'");
+                stmt1.executeUpdate();
+            }
+
+            stmt = (PreparedStatement)con.prepareStatement("SELECT * FROM Player ORDER BY e_points DESC");
+            rs = stmt.executeQuery();
+            rank = 0;
+            while(rs.next()){
+                rank++;
+                stmt1 = (PreparedStatement)con.prepareStatement("UPDATE Player SET nat_rank="+rank+" WHERE username = '"+rs.getString("username")+"'");
+                stmt1.executeUpdate();
+            }
+            
+            try {
                     System.out.println("waiting");
                     con.close();
                     TimeUnit.MINUTES.sleep(1);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(UserUpdater.class.getName()).log(Level.SEVERE, null, ex);
                 }
-            System.out.println("success");
+            
         }catch (SQLException ex) {
                 Logger.getLogger(UserUpdater.class.getName()).log(Level.SEVERE, null, ex);
                 System.exit(1);
